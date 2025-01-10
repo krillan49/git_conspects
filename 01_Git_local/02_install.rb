37b71635c1bab2af2c3aaa912ad794eae0179cbb
krillan <pushkinidze49@gmail.com>
2025-01-10 22:38:33 +0300

    reconstr

---
+++ 01_Git_local/02_install.rb
@@ -0,0 +1,13 @@
+puts '                                  Git(установка и полезные ссылки)'
+
+# Git - в Линукс и Мак стоит по умолчанию
+# https://gitforwindows.org/                             оф сайт чтобы качнуть гит для Винды
+
+# https://github.com/git-guides/install-git              гайд по установке
+# https://think-like-a-git.net/                          описание работы с Git от энтузиастов
+# https://github.com/nodejs/node-gyp#on-windows          команды и разная инфа
+
+# code.visualstudio.com/docs/editor/github
+
+# codeproject.com/Articles/1177391/How-to-Initialize-a-Git-Repository-using-Visual-St
+#=>How to Initialize a Git Repository using Visual Studio Code and Publish the Code to Git Server (Visual Studio Team Services)
