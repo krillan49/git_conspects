35c850472bed2771869da30a339a5973fa89cddc
krillan <pushkinidze49@gmail.com>
2025-01-21 19:13:57 +0300

    jtdjydtjgyufgy

---
+++ 02_GitHub/07_issue.rb
@@ -0,0 +1,5 @@
+puts '                                          GitHub Issue'
+
+# https://docs.github.com/ru/issues/tracking-your-work-with-issues/quickstart
+
+# Заметка по Issue на Github. У issue есть номер (например #12), если при коммите указать этот номер, то issue сама закроектся и пометится как выполненная.
