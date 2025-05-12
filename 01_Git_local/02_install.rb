9a5c4ad275e144d64a95ab7833eba4d23634edc5
krillan <Pushkinidze49@gmail.com>
2025-05-12 21:50:13 +0300

    jgjkjkfhgfg

--- 01_Git_local/02_install.rb
+++ 01_Git_local/02_install.rb
@@ -1,5 +1,6 @@
 puts '                                  Установка и полезные ссылки'
 
+# https://git-scm.com/downloads
 # https://gitforwindows.org/                             оф сайт чтобы качнуть гит для Винды
 # https://github.com/git-guides/install-git              гайд по установке
 # https://think-like-a-git.net/                          описание работы с Git от энтузиастов
